0f209e51982d57fe72e372028899b82b24f95b26
SkySoft8 <skysoft.dmitriy@gmail.com>
2024-03-13 16:34:48 +0300

    update

--- app/Http/Controllers/User/Main/UpdateController.php
+++ app/Http/Controllers/User/Main/UpdateController.php
@@ -15,11 +15,18 @@ class UpdateController extends Controller
     public function __invoke(UpdateRequest $request, User $user)
     {
         $data = $request->validated();
+        $medicals = DB::table('medical_cards')->get();
+        $spa = DB::table('spa_cards')->get();
+        $cafes = DB::table('cafe_cards')->get();
+        $hostels = DB::table('hostel_cards')->get();
+        $sanatoriums = DB::table('sanatorium_cards')->get();
+        $turoperators = DB::table('turoperator_cards')->get();
+        $sports = DB::table('sport_cards')->get();
         $preOrders=  DB::table('pre_orders')->where('user_id', '=', $user['id'])->get();
         $totals = DB::table('pre_orders')->pluck('total_price') ;
         $payments =  DB::table('payments')->where('user_id', '=', $user['id'])->get(); 
         $user->update($data);
         
-        return view('user.main.index' , compact('user' ,'preOrders', 'totals', 'payments'));
+        return view('user.main.index' , compact('user' ,'preOrders', 'totals', 'payments','medicals','sports', 'turoperators', 'sanatoriums', 'hostels','cafes','spa'));
     }
 }
