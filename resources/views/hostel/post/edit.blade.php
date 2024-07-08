567c62f468ba7cd2b41a4997933128a2b26226fe
klimVova <87908301+klimVova@users.noreply.github.com>
2024-07-08 08:33:14 +0300

    asdasd
    asdasd

--- resources/views/hostel/post/edit.blade.php
+++ resources/views/hostel/post/edit.blade.php
@@ -6,7 +6,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-6">
-                        <h1 class="m-0">Редактировани номера </h1>
+                        <h1 class="m-0">Редактирование номера </h1>
                     </div><!-- /.col -->
 
                 </div><!-- /.row -->
